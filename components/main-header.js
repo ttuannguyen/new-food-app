229af51d2001c283e7b95dd08d85df6c0eafd687
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-12-02 18:04:37 -0500

    add links in main header

--- components/main-header.js
+++ components/main-header.js
@@ -1,7 +1,22 @@
 import Link from "next/link";
+import logoImg from '/@assets/logo.png';
 
 export default function MainHeader() {
     return <header>
-        <Link></Link>
+        <Link href="/">
+            <img src={logoImg.src} alt="A plate with food on it" />
+            NextLevel Food
+        </Link>
+
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/meal">Browse Meals</Link>
+                </li>
+                <li>
+                    <Link href="/community">Foodies Community</Link>
+                </li>
+            </ul>
+        </nav>
     </header>
 }
