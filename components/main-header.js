31a03f36e4661407e6f205150d155ac42b8428a9
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-12-03 00:21:31 -0500

    add styling for header

--- components/main-header.js
+++ components/main-header.js
@@ -1,17 +1,19 @@
 import Link from "next/link";
-import logoImg from '/@assets/logo.png';
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+
 
 export default function MainHeader() {
-    return <header>
-        <Link href="/">
+    return <header className={classes.header}>
+        <Link className={classes.logo} href="/">
             <img src={logoImg.src} alt="A plate with food on it" />
             NextLevel Food
         </Link>
 
-        <nav>
+        <nav className={classes.nav}>
             <ul>
                 <li>
-                    <Link href="/meal">Browse Meals</Link>
+                    <Link href="/meals">Browse Meals</Link>
                 </li>
                 <li>
                     <Link href="/community">Foodies Community</Link>
