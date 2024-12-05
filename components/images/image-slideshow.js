a7f0b101ffed944abb46eabc9287418349167b27
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-12-04 22:46:31 -0500

    add content and style the Community page

--- components/images/image-slideshow.js
+++ components/images/image-slideshow.js
@@ -30,7 +30,7 @@ export default function ImageSlideshow() {
       setCurrentImageIndex((prevIndex) =>
         prevIndex < images.length - 1 ? prevIndex + 1 : 0
       );
-    }, 5000);
+    }, 5000); // to rotate the images every 5 secs
 
     return () => clearInterval(interval);
   }, []);
