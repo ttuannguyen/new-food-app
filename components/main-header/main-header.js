0abf68983a8cb9f683adb2394c0106c2f7538352
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-12-10 22:00:20 -0500

    add NavLink component and add it to main header

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,13 +1,11 @@
-'use client';
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
-import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
-  const path = usePathname();
 
   return (
     <>
@@ -21,24 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link
-                href="/meals"
-                className={
-                  path.startsWith("/meals") ? classes.active : undefined
-                }
-              >
-                Browse Meals
-              </Link>
+              <NavLink href='/meals'>Browse Meals</NavLink>
             </li>
             <li>
-              <Link
-                href="/community"
-                className={
-                  path.startsWith("/community") ? classes.active : undefined
-                }
-              >
-                Foodies Community
-              </Link>
+            <NavLink href='/community'>Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
