fd82a9e547f2ecd77501de434dfdc1f7eab7adb5
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-12-10 22:12:01 -0500

    add stylings for nav link

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -1,7 +1,7 @@
 'use client';
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import classes from './nav-link.module.css';
 
 export default function NavLink({href, children}) {
     const path = usePathname();
@@ -10,7 +10,7 @@ export default function NavLink({href, children}) {
         <Link
         href={href}
         className={
-          path.startsWith(href) ? classes.active : undefined
+          path.startsWith(href) ? `${classes.link} ${classes.active}` : classes.link
         }
       >
         {children}
