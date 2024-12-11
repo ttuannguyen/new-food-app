e8d4ef59eee581f8f3c1e02811893a09f770698e
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-12-10 21:48:35 -0500

    add conditional to highlight a path when it's active

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,3 +1,4 @@
+'use client';
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
@@ -7,7 +8,7 @@ import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
   const path = usePathname();
-  
+
   return (
     <>
       <MainHeaderBackground />
@@ -20,10 +21,24 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals" className="">Browse Meals</Link>
+              <Link
+                href="/meals"
+                className={
+                  path.startsWith("/meals") ? classes.active : undefined
+                }
+              >
+                Browse Meals
+              </Link>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <Link
+                href="/community"
+                className={
+                  path.startsWith("/community") ? classes.active : undefined
+                }
+              >
+                Foodies Community
+              </Link>
             </li>
           </ul>
         </nav>
