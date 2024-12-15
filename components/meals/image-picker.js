c5ba8ada5b69e85c69d5f7c6546f50823afd6824
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-12-15 15:10:56 -0500

    changes to ensure that the <form> can't be submitted without an image being selected

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -15,6 +15,7 @@ export default function ImagePicker({ label, name }) {
         const file = event.target.files[0];
         
         if (!file) {
+            setPickedImage(null);
             return;
         }
 
@@ -51,6 +52,7 @@ export default function ImagePicker({ label, name }) {
           ref={imageInput}
           // add 'multiple' prop here if allowing user to upload multiple files
           onChange={handleImageChange}
+          required
         />
       </div>
       <button className={classes.button} type="button" onClick={handlePickClick}>
