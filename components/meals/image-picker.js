ddb0a7edced1fdeaa555fbeeeb65173a4d195ccf
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-12-15 14:47:12 -0500

    add handleImageChange to handle selecting the file to upload

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,19 +1,47 @@
 'use client';
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
 
 export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState();
     const imageInput = useRef(); // React hook giving a ref to connect to an html element   
   
     function handlePickClick() {
         imageInput.current.click();
     }
 
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+        
+        if (!file) {
+            return;
+        }
+
+        const fileReader = new FileReader();
+        
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result); // the generated URL
+        };
+
+        fileReader.readAsDataURL(file);
+
+    }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+            {!pickedImage && <p>No image picked yet.</p>}
+            {pickedImage && (
+                <Image 
+                    src={pickedImage}
+                    alt="The image selected by the user."
+                    fill
+                />
+            )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -21,6 +49,8 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          // add 'multiple' prop here if allowing user to upload multiple files
+          onChange={handleImageChange}
         />
       </div>
       <button className={classes.button} type="button" onClick={handlePickClick}>
